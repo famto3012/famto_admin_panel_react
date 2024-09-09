035a114bbb9d6398ebb074aa29870993bd7a0cf6
its-sarath28 <sarathrs220@gmail.com>
2024-09-09 11:10:20 +0530

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,14 @@ import { StyledEngineProvider } from "@mui/material/styles";
 import { MapProvider } from "./context/MapContext.jsx";
 import { SoundProvider } from "./context/SoundContext.jsx";
 import { SocketProvider } from "./context/SocketContext.jsx";
+import { register } from "./serviceWorker.js";
 
 const theme = createTheme({
   typography: {
     body1: {
       fontSize: "1rem",
     },
-    // Define other typography styles as needed
   },
-  // Define other theme settings as needed
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -37,3 +36,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </StyledEngineProvider>
   </ThemeProvider>
 );
+
+register();
